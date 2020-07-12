152ea5fa6fa7e1082b470134a4342b3e4b0cc6ac
Chinh Vo <chinhvowili@gmail.com>
2020-07-12 16:31:16 +0700

    update script

---
+++ TfsSonarInteraction/scripts/pull-request-decoration.ps1
@@ -0,0 +1,7 @@
+﻿[String]$sourcebranch = "$env:BUILD_SOURCEBRANCH"
+$execFile = "C:\Users\chinh\Downloads\vsts-agent-win7-x64-2.122.1\_work\1\s\TfsSonarInteraction\TfsSonarInteraction.exe";
+$pullrequest = "refs/pull/+(?<pullnumber>\w+?)/merge+"
+if($sourcebranch -match $pullrequest){        
+   Start-Process -FilePath $execFile $Matches.pullnumber, "sonar1" ;
+}
+else { write-host "Cannot find pull request ID" }
---
+++ TfsSonarInteraction/scripts/sonar-scanner.ps1
@@ -0,0 +1,5 @@
+﻿dotnet sonarscanner begin /k:"sonar1"   /d:sonar.login="f5bec4c9e3be0b7444abebed592d3471c6782b29" 
+dotnet build ConsoleApp1/ConsoleApp1.sln
+dotnet build ConsoleApp2/ConsoleApp2.sln 
+
+dotnet sonarscanner end /d:sonar.login="f5bec4c9e3be0b7444abebed592d3471c6782b29" 
